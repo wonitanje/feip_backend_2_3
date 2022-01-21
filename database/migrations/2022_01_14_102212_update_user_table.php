4c0c6a25fd955dd006811ea9b188d295c51f869c
wonitanje <prod.juswee@gmail.com>
2022-01-21 12:02:07 +1000

    Lesson 10: Add web and API auth and profile

---
+++ database/migrations/2022_01_14_102212_update_user_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUser extends Migration
+{
+  public function up()
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->string('login')->unique();
+      $table->string('name')->nullable()->change();
+      $table->string('email')->nullable()->change();
+    });
+  }
+
+  public function down()
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->string('login')->unique();
+      $table->string('name')->nullable()->change();
+      $table->string('email')->nullable()->change();
+    });
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/registration', [ApiAuthController::class, 'registration']);
+Route::post('/login', [ApiAuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [ApiAuthController::class, 'logout']);
+    Route::post('/profile', [ApiAuthController::class, 'profile']);
 });
